661ca28ce59e01be9eeb4250f2c497861960d41b
Sharmina Akhtar <sakht318@gmail.com>
2018-10-29 15:24:51 +0000

    automatically backed up by learn

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  state= { name: ''};
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.name);
+    this.setState({
+      name:'',
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value,
+    })
+  }
   render() {
     return(
       <div>
-        user input
+          <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Restaurant</label>
+          <input type="name" onChange={(event) => this.handleChange(event)} value={this.state.name}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
