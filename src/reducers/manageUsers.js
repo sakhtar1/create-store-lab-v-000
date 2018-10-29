579f03fa49967a1130070797f62908fca121c85a
Sharmina Akhtar <sakht318@gmail.com>
2018-10-29 15:22:21 +0000

    automatically backed up by learn

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,20 @@
-export default function manageUsers(state, action){
+export default function manageUsers(
+  state = {
+       users: []
+   },
+   action
+) {
+   console.log(action);
+   switch (action.type) {
+       case 'ADD_USER':
+           console.log('Current state.users length %s', state.users.length);
+           console.log('Updating state.users length to %s', state.users.length + 1);
+           return Object.assign({}, state, {
+               users: state.users.concat(state.users.length + 1)
+           });
+
+       default:
+           console.log('Initial state.users length: %s', state.users.length);
+           return state;
+   }
 }
